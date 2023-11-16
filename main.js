1f2b85f98bef3c8f6fd886fe14f7f0104739d707
Andrés Garelli <agarelli91@gmail.com>
2023-11-15 21:13:27 -0300

    segunda pre-entrega

--- main.js
+++ main.js
@@ -2,11 +2,49 @@
 class ProductManager {
     constructor () {
         this.products = []
+        this.paht = rutaDB
     }
-    getProducts (){
+
+    async getProducts (){
+        // Variables de la funcion 
+        const productArray = []
+
+        //Recupero de datos
+        if (fs.existsSync(rutaDB)) {
+            const productJson = await fs.promise.readFile(this.paht, "utf-8");
+            productArray = parse(productJson);
+        }
+
+        // Actualizacion de datos.
+        productArray == this.products ?
+        console.log ("los datos estan iguales")
+        : () => {
+            console.log ("los datos son distintos. Los datos de la ram seran igualados a los de la base de datos");
+            this.products = productArray;
+        }
+
+        //Impresion en consola.
         console.log (`La lista de productos completa es ${JSON.stringify(this.products)}`)
     }
-    addProduct ({title, description, price, thumbnail, code, stock}) {
+
+    async addProduct ({title, description, price, thumbnail, code, stock}) {
+        // Variables de la funcion 
+        const productArray = []
+
+        //Recupero de datos
+        if (fs.existsSync(rutaDB)) {
+            const productJson = await fs.promise.readFile(this.paht, "utf-8");
+            productArray = parse(productJson);
+        }
+        
+        // Actualización de datos
+        productArray == this.products ?
+        console.log ("los datos estan iguales")
+        : () => {
+            console.log ("los datos son distintos. Los datos de la ram seran igualados a los de la base de datos");
+            this.products = productArray;
+        }
+
         // code check
         const exist = this.products.filter ((producto) => producto.code == code)
         // id check
@@ -14,54 +52,98 @@ class ProductManager {
        // Product set
         exist.length > 0 ? 
             console.log ("El code ingresado ya se encuentra en la base de datos") 
-            : this.products.push ({title, description, price, thumbnail, code, stock, id: idlog + 1/*seteo id*/})
+            : async () => { /* funcion asyncronica anonima que agrega nuevo producto al array*/
+                this.products.push ({title, description, price, thumbnail, code, stock, id: idlog + 1/*seteo id*/})
+                nuevoArray = stringify (this.products)
+                await fs.promise.writeFile (rutaDB, nuevoArray)
+            }
     }
-    getProductById (idDB){
+    async getProductById (idDB){
+        // Variables de la funcion 
+        const productArray = []
+
+        //Recupero de datos
+        if (fs.existsSync(rutaDB)) {
+            const productJson = await fs.promise.readFile(this.paht, "utf-8");
+            productArray = parse(productJson);
+        }
+        
+        // Actualización de datos
+        productArray == this.products ?
+        console.log ("los datos estan iguales")
+        : () => {
+            console.log ("los datos son distintos. Los datos de la ram seran igualados a los de la base de datos");
+            this.products = productArray;
+        }
         // Product Search
-        const productFound = this.products.filter ((producto) => producto.id == idDB)
-       // Search set
+        const productFound = this.products.filter ((producto) => producto.id == idDB);
+       // aSearch set
         productFound.length > 0 ? 
-            console.log (`El producto que solicito por id fue ${JSON.stringify(productFound)}`) 
+            console.log (`El producto que solicito por id fue ${JSON.stringify(productFound)}`)
             : console.log ("Not found");
     }
+
+    async updateProductById (id, productoObjetc){
+        // Variables de la funcion 
+        const productArray = []
+
+        //Recupero de datos
+        if (fs.existsSync(rutaDB)) {
+            const productJson = await fs.promise.readFile(this.paht, "utf-8");
+            productArray = parse(productJson);
+        }
+        
+        // Actualización de datos
+        productArray == this.products ?
+        console.log ("los datos estan iguales")
+        : () => {
+            console.log ("los datos son distintos. Los datos de la ram seran igualados a los de la base de datos");
+            this.products = productArray;
+        }
+
+        // Product Search
+       const productSelect = this.products.findIndex ((producto) => producto.id == id)
+       // Search set
+       productSelect.length > 0 ? 
+            async () => { /* funcion asincrona anonima para ejecutar si hay productSelect*/
+            this.products[productSelect] = {...this.products[productSelect], ...productoObjetc}
+            await fs.promise.writeFile(rutaDB, this.products)
+            }
+            : console.log ("Not found");
+    }
+    
+    async deletProductById (idDB) {
+        // Variables de la funcion 
+        const productArray = []
+
+        //Recupero de datos
+        if (fs.existsSync(rutaDB)) {
+            const productJson = await fs.promise.readFile(this.paht, "utf-8");
+            productArray = parse(productJson);
+        }
+        
+        // Actualización de datos
+        productArray == this.products ?
+        console.log ("los datos estan iguales")
+        : () => {
+            console.log ("los datos son distintos. Los datos de la ram seran igualados a los de la base de datos");
+            this.products = productArray;
+        }
+
+        // Product Search
+       const productSelect = this.products.findIndex ((producto) => producto.id == idDB)
+       // Search set
+       productSelect.length > 0 ? 
+            async () => { /* funcion asincrona anonima para ejecutar si hay productSelect*/
+            this.products.slice(productSelect)
+            await fs.promise.writeFile(rutaDB, this.products)
+            }
+            : console.log ("Not found product you want to delete");
+    }
+
 }
 
 // Creacion de instancia de clase productArray
-const productArray = new ProductManager ();
+const productArray = new ProductManager ("./productosEnlistados.json");
 
 // Código para prueba por consola de funcionalidades requeridas.
-
-console.log (productArray); /* Creacion de la instancia vacia []*/
-
-productArray.addProduct (
-{title: "producto prueba", 
-description: "Este es un producto prueba", 
-price: 200, 
-thumbnail: "Sin imagen",
-code: "abc123",
-stock: 25}
-);
-console.log (productArray); /* Agruegado de productos con objeto indicado */
-
-
-productArray.addProduct (
-    {title: "producto prueba 2", 
-    description: "Este es un producto prueba", 
-    price: 301, 
-    thumbnail: "Sin imagen",
-    code: "def456",
-    stock: 25}
-);
-
-productArray.getProducts(); /* Obtener lista de productos completa */
-
-productArray.getProductById(2); /* Obtener lista de producto seleccionado por id*/
-
-productArray.addProduct (
-    {title: "producto prueba 2", 
-    description: "Este es un producto prueba", 
-    price: 301, 
-    thumbnail: "Sin imagen",
-    code: "def456",
-    stock: 25}
-); /* control de que no se repita el code*/
